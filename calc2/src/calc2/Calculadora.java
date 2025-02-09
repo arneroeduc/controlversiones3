6819058e52243e0b239620fcf5238f301ffc9f30
arner0 <arner0@localhost>
2025-02-09 12:49:07 +0100

    ultima version

--- calc2/src/calc2/Calculadora.java
+++ calc2/src/calc2/Calculadora.java
@@ -14,7 +14,7 @@ public class Calculadora {
 	public int multiplicar(int a, int b) {
 	    return a * b;
 	}
-//rama nueva funcionalidad
+//rama MAIN
 	 
 	 public static void main(String[] args) {
 		 
